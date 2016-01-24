5f0b1d0329ed0e3be5e9daccdbded32868f0697d
Priya Manavalan <priya.manavalan@jhuapl.edu>
2016-01-24 16:32:38 +0000

    Added settings.py

--- boss/settings.py
+++ boss/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'bossmeta',
 )
 
 MIDDLEWARE_CLASSES = (
