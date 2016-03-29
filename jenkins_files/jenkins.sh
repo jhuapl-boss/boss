d1c00dba069fab3110a6e69b7fa65f7eef1fede4
Tim Gion <timothy.gion@jhuapl.edu>
2016-03-29 15:17:42 -0400

    Start DynamoDB earlier to ensure it's ready when tests begin.

--- jenkins_files/jenkins.sh
+++ jenkins_files/jenkins.sh
@@ -6,6 +6,9 @@
 # settings.py disables some dependencies when this env variable is set.
 export USING_DJANGO_TESTRUNNER=1
 
+# Start local DynamoDB.
+java -Djava.library.path=/usr/local/bin/dynamo/DynamoDBLocal_lib/ -jar /usr/local/bin/dynamo/DynamoDBLocal.jar -inMemory &
+
 # Ensure a fresh DB available.
 mysql -u root --password=MICrONS < jenkins_files/fresh_db.sql
 
@@ -17,18 +20,14 @@ rm -rf */migrations
 python3 manage.py makemigrations --noinput
 
 # Force create migrations for the bosscore app.
-python3 manage.py makemigrations bosscore --noinput 
+python3 manage.py makemigrations bosscore --noinput
 
 python3 manage.py migrate
 
 python3 manage.py collectstatic --noinput
-
-# Start local DynamoDB.
-java -Djava.library.path=/usr/local/bin/dynamo/DynamoDBLocal_lib/ -jar /usr/local/bin/dynamo/DynamoDBLocal.jar -inMemory &
 
 # Run tests.
 python3 manage.py jenkins --enable-coverage --noinput
 
 # Shutdown local DynamoDB.
 kill $!
-
