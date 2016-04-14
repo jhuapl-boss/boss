2e2c32f9195c15bbe296f80899a07360e827f1e6
Tim Gion <timothy.gion@jhuapl.edu>
2016-04-14 10:01:28 -0400

    Pointed wsgi.py at boss.settings.production. settings is now a module with files for each config.

--- django/boss/wsgi.py
+++ django/boss/wsgi.py
@@ -26,6 +26,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boss.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boss.settings.production")
 
 application = get_wsgi_application()
