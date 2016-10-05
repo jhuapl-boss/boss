23d60837ed320bd3aa7ccade0bff70370f084a22
Kleissas, Dean M <dean.kleissas@jhuapl.edu>
2016-10-05 18:14:18 -0400

    minor update to pass the boss resource for an ingest job on a GET

--- django/bossingest/views.py
+++ django/bossingest/views.py
@@ -4,9 +4,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from bosscore.error import BossError
+from bosscore.error import BossError, ErrorCodes
 from bossingest.ingest_manager import IngestManager
 from bossingest.serializers import IngestJobListSerializer
+from bosscore.models import Collection, Experiment, ChannelLayer
 
 import bossutils
 from bossutils.ingestcreds import IngestCredentials
@@ -32,6 +33,7 @@ class IngestJobView(APIView):
             ingest_job = ingest_mgmr.get_ingest_job(ingest_job_id)
             serializer = IngestJobListSerializer(ingest_job)
 
+            # Start setting up output
             data = {}
             data['ingest_job'] = serializer.data
             data['tile_bucket_name'] = ingest_mgmr.get_tile_bucket()
@@ -46,9 +48,56 @@ class IngestJobView(APIView):
             config = bossutils.configuration.BossConfig()
             data['ingest_lambda'] = config["lambda"]["page_in_function"]
 
+            # Generate a "resource" for the ingest lambda function to be able to use SPDB cleanly
+            collection = Collection.objects.get(name=data['ingest_job']["collection"])
+            experiment = Experiment.objects.get(name=data['ingest_job']["experiment"], collection=collection)
+            channel_layer = ChannelLayer.objects.get(name=data['ingest_job']["channel_layer"], experiment=experiment)
+
+            resource = {}
+            resource['boss_key'] = ['{}&{}&{}'.format(data['ingest_job']["collection"],
+                                                      data['ingest_job']["experiment"],
+                                                      data['ingest_job']["channel_layer"])]
+            resource['lookup_key'] = ['{}&{}&{}'.format(collection.id,
+                                                        experiment.id,
+                                                        channel_layer.id)]
+            resource['collection'] = {}
+            resource['collection']['name'] = collection.name
+            resource['collection']['description'] = collection.description
+            resource['coord_frame'] = {}
+            resource['coord_frame']['name'] = ""
+            resource['coord_frame']['description'] = ""
+            resource['coord_frame']['x_start'] = 0
+            resource['coord_frame']['x_stop'] = 0
+            resource['coord_frame']['y_start'] = 0
+            resource['coord_frame']['y_stop'] = 0
+            resource['coord_frame']['z_start'] = 0
+            resource['coord_frame']['z_stop'] = 0
+            resource['coord_frame']['x_voxel_size'] = 0
+            resource['coord_frame']['y_voxel_size'] = 0
+            resource['coord_frame']['z_voxel_size'] = 0
+            resource['coord_frame']['voxel_unit'] = "nanometers"
+            resource['coord_frame']['time_step'] = 0
+            resource['coord_frame']['time_step_unit'] = "nm"
+            resource['experiment'] = {}
+            resource['experiment']['name'] = experiment.name
+            resource['experiment']['description'] = experiment.description
+            resource['experiment']['num_hierarchy_levels'] = experiment.num_hierarchy_levels
+            resource['experiment']['hierarchy_method'] = experiment.hierarchy_method
+            resource['experiment']['max_time_sample'] = experiment.max_time_sample
+            resource['channel_layer'] = {}
+            resource['channel_layer']['name'] = channel_layer.name
+            resource['channel_layer']['description'] = channel_layer.description
+            resource['channel_layer']['is_channel'] = channel_layer.is_channel
+            resource['channel_layer']['datatype'] = channel_layer.datatype
+
+            # Set resource
+            data['resource'] = resource
+
             return Response(data, status=status.HTTP_200_OK)
         except BossError as err:
                 return err.to_http()
+        except Exception as err:
+            return BossError("{}".format(err), ErrorCodes.BOSS_SYSTEM_ERROR).to_http()
 
     def post(self,request):
         """
