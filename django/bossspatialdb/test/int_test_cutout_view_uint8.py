38976f9ce42d20ef074ce931166943107517605e
Kleissas, Dean M <dean.kleissas@jhuapl.edu>
2016-11-04 17:44:41 -0400

    bugfix to cross-account reuse of bucket names during integration testing

--- django/bossspatialdb/test/int_test_cutout_view_uint8.py
+++ django/bossspatialdb/test/int_test_cutout_view_uint8.py
@@ -21,6 +21,7 @@ from rest_framework import status
 import numpy as np
 
 from bossspatialdb.test import CutoutInterfaceViewUint8TestMixin
+from bossspatialdb.test.params import *
 from bossspatialdb.views import Cutout
 
 from bosscore.test.setup_db import SetupTestDB
@@ -28,31 +29,10 @@ import bossutils
 import redis
 import time
 import blosc
+import random
 
 from spdb.spatialdb.test.setup import SetupTests
 from botocore.exceptions import ClientError
-
-version = settings.BOSS_VERSION
-
-config = bossutils.configuration.BossConfig()
-KVIO_SETTINGS = {"cache_host": config['aws']['cache'],
-                 "cache_db": 1,
-                 "read_timeout": 86400}
-
-# state settings
-STATEIO_CONFIG = {"cache_state_host": config['aws']['cache-state'],
-                  "cache_state_db": 1}
-
-# object store settings
-OBJECTIO_CONFIG = {"s3_flush_queue": None,
-                   "cuboid_bucket": "intTest.{}".format(config['aws']['cuboid_bucket']),
-                   "page_in_lambda_function": config['lambda']['page_in_function'],
-                   "page_out_lambda_function": config['lambda']['flush_function'],
-                   "s3_index_table": "intTest.{}".format(config['aws']['s3-index-table'])}
-
-config = bossutils.configuration.BossConfig()
-_, domain = config['aws']['cuboid_bucket'].split('.', 1)
-FLUSH_QUEUE_NAME = "intTest.S3FlushQueue.{}".format(domain).replace('.', '-')
 
 
 @override_settings(KVIO_SETTINGS=KVIO_SETTINGS)
