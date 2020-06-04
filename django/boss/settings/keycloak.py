4069539654418dd4ba752b093b02ee15480882a0
Tim Gion <movestill@gmail.com>
2020-06-04 15:52:57 -0400

    Login/logout working
    Use POST for logout.
    Need to get public key from Keycloak via automation.

--- django/boss/settings/keycloak.py
+++ django/boss/settings/keycloak.py
@@ -13,10 +13,15 @@
 # limitations under the License.
 
 from .mysql import *
+from bossoidc.settings import BOSSOIDC_LOGIN_URL, BOSSOIDC_LOGOUT_URL
 
 """
 Run Boss with a local Keycloak instance for testing.
 """
+
+# Set this here so it's not overriden by any other settings files.
+LOGIN_URL = BOSSOIDC_LOGIN_URL
+LOGOUT_URL = BOSSOIDC_LOGOUT_URL
 
 INSTALLED_APPS.append("bossoidc")
 INSTALLED_APPS.append("mozilla_django_oidc")
@@ -29,18 +34,36 @@ REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = (
     'oidc_auth.authentication.BearerTokenAuthentication',
 )
 
-AUTHENTICATION_BACKENDS.insert(1, 'mozilla_django_oidc.auth.OIDCAuthenticationBackend') 
+#AUTHENTICATION_BACKENDS.insert(1, 'mozilla_django_oidc.auth.OIDCAuthenticationBackend') 
 AUTHENTICATION_BACKENDS.insert(1, 'bossoidc.backend.OpenIdConnectBackend') 
 
-auth_uri = 'http://localhost:8080'
+auth_uri = 'http://localhost:8080/auth/realms/BOSS'
 client_id = 'endpoint'
 public_uri = 'http://localhost:8000'
 
 OIDC_OP_AUTHORIZATION_ENDPOINT = auth_uri + '/protocol/openid-connect/auth'
 OIDC_OP_TOKEN_ENDPOINT = auth_uri + '/protocol/openid-connect/token'
 OIDC_OP_USER_ENDPOINT = auth_uri + '/protocol/openid-connect/userinfo'
-OIDC_LOGIN_REDIRECT_URL = public_uri
+#LOGIN_REDIRECT_URL = public_uri + '/openid/callback/login/'
+LOGIN_REDIRECT_URL = public_uri + 'v1/mgmt'
+LOGOUT_REDIRECT_URL = auth_uri + '/protocol/openid-connect/logout'
 OIDC_RP_CLIENT_ID = client_id
+OIDC_RP_CLIENT_SECRET = ''
+OIDC_RP_SCOPES = 'sub preferred_username'
+OIDC_RP_SIGN_ALGO = 'RS256'
+OIDC_RP_IDP_SIGN_KEY = (
+'''-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApgdDYs/B+6XJfuo6mCFW
+/JTafaWE9WcDqZd8jB3rj+DEeZuUg+5wS34ih7YAgv/8L09FAL/rdX2DBxS1Ohmo
+m5Lu4qmZVHy2Xwu6NHVwozWydilldlvu6vAa9ozMtlNNuTTDTyEgCxLbXHEMEpRd
+xVcy5P0kEY2YyvLXAKK5iX+XEnIH3IFbzPBvqwBGneIkGXEYPszXFELaZ2bp0vPI
+ZfFdlb4Nv4FwodLNuKLjEoXLHHscoR3zmuzeq5cJ+i2mzInn4kkEdBXHoADTUvot
+qMNblmEMW8muwAd4i7e4nvfnSRjq7+bPlb1jEOBDL6nd8cU4ACb/p44k0rBggtFV
+aQIDAQAB
+-----END PUBLIC KEY-----''')
+OIDC_OP_JWKS_ENDPOINT = auth_uri + '/protocol/openid-connect/certs'
+# No SSL when running locally during development.
+OIDC_VERIFY_SSL = False
 
 from bossoidc.settings import configure_oidc
 configure_oidc(auth_uri, client_id, public_uri)
