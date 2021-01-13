0e2aab115bef4b86d6294413d65b89621fdf285e
Daniel Xenes <daniel.xenes@jhuapl.edu>
2021-01-13 15:35:52 -0500

    makes ng link dynamic

--- django/mgmt/static/js/experiments.js
+++ django/mgmt/static/js/experiments.js
@@ -3,7 +3,9 @@ function channel_handler(response) {
     var detail_base_url = API_ROOT + "mgmt/resources/" + resources[0] + "/" + resources[1] + "/";
     var delete_function = "delete_channel";
     var delete_base_url = API_ROOT + "collection/" + resources[0] + "/experiment/" + resources[1] + "/channel/";
-    var neuroglancer_url = "https://neuroglancer.bossdb.io/#!{'layers':{'" + resources[1] + "':{'source':'boss://https://" + window.location.host + "/" + resources[0] + "/" + resources[1] + "/";
+
+    var neuroglancer_base_url = "https://neuroglancer." + window.location.host.split('.')[1] + ".io/"
+    var neuroglancer_url = neuroglancer_base_url + "#!{'layers':{'" + resources[1] + "':{'source':'boss://https://" + window.location.host + "/" + resources[0] + "/" + resources[1] + "/";
 
     return channel_resource_formatter(response, detail_base_url, delete_function, delete_base_url, neuroglancer_url);
 }
