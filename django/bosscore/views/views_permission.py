0e21916c3357f3c20feffaf4f11a6ec2f96f331d
Daniel Xenes <daniel.xenes@jhuapl.edu>
2023-02-13 13:58:17 -0500

    fix public group post

--- django/bosscore/views/views_permission.py
+++ django/bosscore/views/views_permission.py
@@ -242,7 +242,7 @@ class ResourceUserPermission(APIView):
 
         try:
             # public group can only have read permission
-            if group_name == PUBLIC_GRP and not (set(perm_list).issubset({'read', 'read_volumetric_data'})):
+            if group_name == PUBLIC_GRP and not (set(perm_list).issubset({'read', 'read_volumetric_data', 'read_metadata'})):
                 return BossHTTPError("The public group can only have read permissions",
                                      ErrorCodes.INVALID_POST_ARGUMENT)
 
@@ -299,7 +299,7 @@ class ResourceUserPermission(APIView):
 
         try:
             # public group can only have read permission
-            if group_name == PUBLIC_GRP and (len(perm_list) != 1 or perm_list[0] != 'read'):
+            if group_name == PUBLIC_GRP and (set(perm_list).issubset({'read', 'read_volumetric_data', 'read_metadata'})):
                 return BossHTTPError("The public group can only have read permissions",
                                      ErrorCodes.INVALID_POST_ARGUMENT)
             # If the user is not a member or maintainer of the group, they cannot patch permissions
